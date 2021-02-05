d494669415910b5eb4ecaeeb2ad0bd45f597dd24
Kowaine <kowaine@qq.com>
2021-02-06 02:17:44 +0800

    测试自动部署

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 @Author: Kowaine
 @Description: 测试travis
 @Date: 2021-01-20 02:59:18
-@LastEditTime: 2021-02-06 01:58:40
+@LastEditTime: 2021-02-06 02:15:51
 """
 
 import requests
 
 # do nothing
 with open("test.txt", "w") as f:
+    pass
+
+with open("test2.txt", "w") as f:
     pass
