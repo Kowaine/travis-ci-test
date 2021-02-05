bd2cf69a6ed245ecaaded9d17c05ed01f4a00b9e
Kowaine <kowaine@qq.com>
2021-02-06 01:59:18 +0800

    测试是否能改变仓库内文件

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 @Author: Kowaine
 @Description: 测试travis
 @Date: 2021-01-20 02:59:18
-@LastEditTime: 2021-01-20 02:59:44
+@LastEditTime: 2021-02-06 01:58:40
 """
 
 import requests
 
 # do nothing
+with open("test.txt", "w") as f:
+    pass
