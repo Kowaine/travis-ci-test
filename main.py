55c59072ce8c128f990492dc43d3ab073267a9f7
Kowaine <kowaine@qq.com>
2021-02-06 02:24:13 +0800

    debug

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 @Author: Kowaine
 @Description: 测试travis
 @Date: 2021-01-20 02:59:18
-@LastEditTime: 2021-02-06 02:15:51
+@LastEditTime: 2021-02-06 02:23:51
 """
 
 import requests
 
 # do nothing
-with open("test.txt", "w") as f:
+with open("data/test.txt", "w") as f:
     pass
 
-with open("test2.txt", "w") as f:
+with open("data/test2.txt", "w") as f:
     pass
